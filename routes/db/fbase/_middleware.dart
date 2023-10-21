a40cb7e1bac0b919ebe588360502f319cbefcae5
myominlatt <myominlatt330@gmail.com>
2023-10-21 10:00:43 +0630

    Dart Frog Full Course 2023 Tutorial #9 - Firebase Integration

---
+++ routes/db/fbase/_middleware.dart
@@ -0,0 +1,14 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:firedart/firedart.dart';
+
+Handler middleware(Handler handler) {
+  return (context) async {
+    if (!Firestore.initialized) {
+      Firestore.initialize('tasklist-dartfrog-f6e2a');
+    }
+
+    final response = await handler(context);
+
+    return response;
+  };
+}
