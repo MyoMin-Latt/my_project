acde86ca3cadd2734ec20ea62059090c0f2c92ad
myominlatt <myominlatt330@gmail.com>
2023-10-29 16:05:43 +0630

    change local db

--- routes/db/mongodb/_middleware.dart
+++ routes/db/mongodb/_middleware.dart
@@ -3,9 +3,9 @@ import 'package:mongo_dart/mongo_dart.dart';
 
 Handler middleware(Handler handler) {
   return (context) async {
-    final db = await Db.create(
-        'mongodb+srv://dartfrog:dartfrogpassword@cluster0.gwlsug7.mongodb.net/TasklistDfrog?retryWrites=true&w=majority');
-    // final db = Db('mongodb://localhost:27017/mongo_dart-blog');
+    // final db = await Db.create(
+    //     'mongodb+srv://dartfrog:dartfrogpassword@cluster0.gwlsug7.mongodb.net/TasklistDfrog?retryWrites=true&w=majority');
+    final db = Db('mongodb://localhost:27017/names');
     print('DB Connect? : ${db.isConnected}');
     if (!db.isConnected) {
       await db.open();
